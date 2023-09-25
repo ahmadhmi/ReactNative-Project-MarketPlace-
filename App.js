7887fb98da5da4b5633c21b0829ad2890013ebd2
Ahmad Heshmati <amd.heshmati@gmail.com>
2023-09-25 14:57:09 -0600

    Initial  commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1}  onPress={handlePress}>
+        Hello World! blah blah blah blah blah blah blah blah blah blah blah blah
+        blah
+      </Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
+    alignItems: "center",
   },
 });
