07bc0e28b9882a482e0b511ecd2e982c39f26750
Ahmad Heshmati <amd.heshmati@gmail.com>
2023-10-01 17:15:13 -0600

    designing Welcome and View Image screens

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View} from "react-native";
-
+import { View, image, text } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 export default function App() {
-  const handlePress = () => console.log("Text pressed");
   return (
-    <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1}  onPress={handlePress}>
-        Hello World! blah blah blah blah blah blah blah blah blah blah blah blah
-        blah
-      </Text>
-    </SafeAreaView>
+    <>
+      <ViewImageScreen/>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "dodgerblue",
-    alignItems: "center",
-  },
-});
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/logo-red.png")} style={styles.logo} />
+        <Text>Sell Anything You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: { flex: 1, justifyContent: "flex-end" },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignSelf: "center",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
+
+export default WelcomeScreen;
